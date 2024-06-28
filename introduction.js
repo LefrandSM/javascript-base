ba1042091a4ec4b23a0b2f9a49da38283d46a386
LefrandSM <lefrandseptiandm@gmail.com>
2024-06-28 16:34:38 +0200

    introduction

---
+++ introduction.js
@@ -0,0 +1,43 @@
+// 6 Data Types in Javascript
+// number, string, boolean, object, function, undifined
+
+
+// JavaScript Can Change HTML Content
+// document.getElementById("button").innerHTML = "Lefrand";
+
+// JavaScript Can Change HTML Attribute Values
+// document.getElementById("navbar-dark").id = "navbar-light";
+// document.getElementById("navbar-dark").id = "navbar-light";
+
+// JavaScript Can Change HTML Styles (CSS)
+// let coba = document.getElementById("button");
+// coba.style.fontSize = "3em";
+
+// JavaScript Can Hide/Show HTML Elements
+// let btn = document.querySelector(".disappear-btn");
+// btn.addEventListener("click", () => {
+//     let hide = document.querySelector("header#hide");
+//     let header = document.querySelector("header");
+//     if (hide == null) {
+//         header.id = "hide";
+//     } else {
+//         header.removeAttribute("id");
+//     }
+// });
+
+// The <script> Tag
+// In HTML, JavaScript code is inserted between <script> and </script> tags.
+
+// JavaScript in <head> or <body></body>
+// Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+
+// External JavaScript
+// It separates HTML and code
+// It makes HTML and JavaScript easier to read and maintain
+// Cached JavaScript files can speed up page loads
+
+// External References
+// With a full URL (a full web address)
+// With a file path (like /js/)
+// Without any path
+
